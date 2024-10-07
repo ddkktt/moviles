455b3c0aa7a09ce41cfb53f72d630bed4313c18a
ddkktt <128318872+ddkktt@users.noreply.github.com>
2024-10-07 15:48:45 -0600

    lista de paquetes

--- entregafinal/lib/screens/packageList.dart
+++ entregafinal/lib/screens/packageList.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
-
+import 'packageData.dart';
 class PackageListScreen extends StatelessWidget {
-  const PackageListScreen({super.key});
+  const PackageListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  const Text("to do");
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mis paquetes'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 16,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: ListTile(
+                    title: const Text('Mis paquetes'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const PackageDataScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+          const BottomNavigationBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomNavigationBar extends StatelessWidget {
+  const BottomNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border(top: BorderSide(color: Colors.grey.shade300)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.add_box_outlined),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.account_circle),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
