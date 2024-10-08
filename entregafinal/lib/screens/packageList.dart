db3c7a523364511317539d514e2c85b425579c6a
ddkktt <128318872+ddkktt@users.noreply.github.com>
2024-10-08 07:47:01 -0600

    lista

--- entregafinal/lib/screens/packageList.dart
+++ entregafinal/lib/screens/packageList.dart
@@ -32,38 +32,6 @@ class PackageListScreen extends StatelessWidget {
               },
             ),
           ),
-          const BottomNavigationBar(),
-        ],
-      ),
-    );
-  }
-}
-
-class BottomNavigationBar extends StatelessWidget {
-  const BottomNavigationBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      decoration: BoxDecoration(
-        border: Border(top: BorderSide(color: Colors.grey.shade300)),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.add_box_outlined),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.account_circle),
-            onPressed: () {},
-          ),
         ],
       ),
     );
