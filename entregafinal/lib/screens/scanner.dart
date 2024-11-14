6a072713bdd71878f5b3b48064657ef0a02486b3
Froylan Cisneros Alvizo <f.froylan11@gmail.com>
2024-11-14 16:33:59 -0600

    Se agrego la camara para el scan de QR y se agrego un boton en configuracion para obtener el token del dispositivo. el codigo proyecto ya esta listo para poner una notificacion desde firebase

--- entregafinal/lib/screens/scanner.dart
+++ entregafinal/lib/screens/scanner.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_scan_barcode/flutter_barcode_scan.dart';
 
-class ScannerScreen extends StatelessWidget{
+class ScannerScreen extends StatefulWidget {
   const ScannerScreen({super.key});
-  
+
+  @override
+  _ScannerScreenState createState() => _ScannerScreenState();
+}
+
+class _ScannerScreenState extends State<ScannerScreen> {
+  String _scanResult = 'No scan result';
+
+  Future<void> _scanQRCode() async {
+    try {
+      final result = await FlutterBarcodeScan.scanBarcode(
+        "#ff6666", // color del borde del escáner
+        "Cancelar", // texto del botón de cancelación
+        true, // muestra el icono de flash
+        ScanMode.QR, // escaneo en modo QR
+      );
+
+      if (result != "-1") {
+        setState(() {
+          _scanResult = result; // El resultado escaneado
+        });
+      } else {
+        setState(() {
+          _scanResult = "Escaneo cancelado"; // Si el escaneo es cancelado
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _scanResult = "Error: $e"; // Si ocurre un error en el escaneo
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        width: 200,
-        height: 200,
+        width: 300, // Puedes ajustar el tamaño del contenedor
+        height: 300,
         decoration: BoxDecoration(
           border: Border.all(
             color: Colors.blue,
@@ -16,15 +49,30 @@ class ScannerScreen extends StatelessWidget{
           ),
           borderRadius: BorderRadius.circular(10),
         ),
-        child: const Center(
-          child: Text(
-            'QR Scanner',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: GestureDetector(
+                onTap: _scanQRCode, // Inicia el escaneo cuando se toque el contenedor
+                child: Center(
+                  child: Text(
+                    'Scan QR Code',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
             ),
-          ),
+            SizedBox(height: 10),
+            Text(
+              'Scan Result: $_scanResult',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
     );
